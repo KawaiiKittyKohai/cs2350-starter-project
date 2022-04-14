0cb473a1cf11e81f27faf7ffb9244e6d362d1c1e
KawaiiKittyKohai <98370539+KawaiiKittyKohai@users.noreply.github.com>
2022-04-13 18:58:25 -0600

    Final submission

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,3 +5,31 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 //TODO - Your ES6 JavaScript code (if any) goes here
 import 'bootstrap'
+
+
+function render() {
+    let cvs = document.querySelector('canvas')
+
+    if (cvs.getContext) {
+        let ctx = cvs.getContext('2d')
+
+        // Clearing face
+        // ctx.clearRect()
+
+        // Head
+        // ctx.fillStyle = document.getElementById('c').value
+        ctx.fillStyle = "yellow"
+        ctx.beginPath()
+        ctx.arc(400, 200, 75, 0, 2 * Math.PI)
+        ctx.fill()
+
+        // Smile
+        ctx.moveTo(425, 200)
+        ctx.arc(200, 200, 15, 0, Math.PI, false)
+
+
+
+
+
+    }
+}
